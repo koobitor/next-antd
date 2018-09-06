414c4b770d137642e20b458b06ce10edb4865bf6
Sakol Assawasagool <koobitor@gmail.com>
2018-09-06 10:32:28 +0700

    next-antd

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>Default</Button>
+      </div>
+    )
+  }
+}
+
+export default Index
